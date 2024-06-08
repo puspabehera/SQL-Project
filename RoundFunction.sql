3a3938f507d44dc9b84a13a0baa5ca721f1533c4
puspa behera <puspa.dbdev@gmail.com>
2024-06-08 19:38:42 +0530

    RoundFunction

---
+++ RoundFunction.sql
@@ -0,0 +1,8 @@
+SELECT ROUND(1.756,0)   --2.000
+SELECT ROUND(1.46,0)    --1.00
+SELECT ROUND(123.45,-1) --120.00
+SELECT ROUND(126.45,-1) --130.00
+SELECT ROUND(555.00,-2) --600.00
+SELECT ROUND(123.5,0) -- 124.0 -- (0) will Round in form of 1.
+SELECT ROUND(123.1,-1)-- 120.0 --(-1) will Round in form of 10.
+SELECT ROUND(123.1,-2)--100.0 -- (-2) will Round in form of 100.
